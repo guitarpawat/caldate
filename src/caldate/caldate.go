9067559d4fd79e3213c04eca17300ffb8dfc9e4f
Dearrays <dear_180771@hotmail.com>
2018-06-26 15:50:02 +0700

    [Edited] เพิ่ม function UnitWeek กับ Test ของ Function นี้

--- src/caldate/caldate.go
+++ src/caldate/caldate.go
@@ -1,1 +1,14 @@
 package caldate
+
+import "strconv"
+
+func UnitWeek(targetDate int) string {
+	totalWeek := targetDate / 7
+	totalDay := targetDate % 7
+	if totalDay > 0 {
+		result1 := strconv.Itoa(totalWeek) + " weeks and " + strconv.Itoa(totalDay) + " days"
+		return result1
+	}
+	result2 := strconv.Itoa(totalWeek) + " weeks"
+	return result2
+}
--- src/caldate/caldate_test.go
+++ src/caldate/caldate_test.go
@@ -1,1 +1,22 @@
 package caldate
+
+import (
+	"testing"
+)
+
+func Test_UnitWeek_Input_187_Should_Be_26_Weeks_And_5_Days(t *testing.T) {
+	expected := "26 weeks"
+	targetDate := 182
+	actualDate := UnitWeek(targetDate)
+	if actualDate != expected {
+		t.Errorf("expected %s but got %s", expected, actualDate)
+	}
+}
+func Test_UnitWeek_Input_182_Should_Be_26_Weeks_And_5_Days(t *testing.T) {
+	expected := "26 weeks and 5 days"
+	targetDate := 187
+	actualDate := UnitWeek(targetDate)
+	if actualDate != expected {
+		t.Errorf("expected %s but got %s", expected, actualDate)
+	}
+}
