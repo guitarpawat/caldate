e1da177bfe71510de8c48c103341858a9e155059
kakmond <wongsathorn.pa@ku.th>
2018-06-26 15:49:36 +0700

    เพิ่ม function convertToMin และ Test

--- src/caldate/caldate.go
+++ src/caldate/caldate.go
@@ -3,3 +3,7 @@ package caldate
 func convertToSecond(days int) uint64 {
 	return uint64(days * 86400)
 }
+
+func convertToMin(second uint64) uint64 {
+	return second / 60
+}
--- src/caldate/caldate_test.go
+++ src/caldate/caldate_test.go
@@ -13,3 +13,13 @@ func Test_ConvertToSecond_Input_182_Should_Be_15724800(t *testing.T) {
 		t.Errorf("expected %d but got %d", uint64(expected), uint64(actual))
 	}
 }
+
+func Test_ConvertToMin_Input_15724806_Should_Be_262080(t *testing.T) {
+	expected := uint64(262080)
+	second := uint64(15724806)
+	actual := uint64(convertToMin(second))
+
+	if actual != expected {
+		t.Errorf("expected %d but got %d", uint64(expected), uint64(actual))
+	}
+}
