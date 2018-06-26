7112855c516d311fc15d8b9c0dc858fa5bbeba61
kakmond <wongsathorn.pa@ku.th>
2018-06-26 15:21:53 +0700

    [Edited]: สร้าง function convertToSecond และสร้าง Test

--- src/caldate/caldate.go
+++ src/caldate/caldate.go
@@ -1,1 +1,5 @@
 package caldate
+
+func convertToSecond(days int) int {
+	return days * 86400
+}
--- src/caldate/caldate_test.go
+++ src/caldate/caldate_test.go
@@ -1,1 +1,15 @@
 package caldate
+
+import (
+	"testing"
+)
+
+func Test_ConvertToSecond_Input_182_Should_Be_15724800(t *testing.T) {
+	expected := 15724800
+	days := 182
+	actual := convertToSecond(days)
+
+	if actual != expected {
+		t.Errorf("expected %d but got %d", expected, actual)
+	}
+}
