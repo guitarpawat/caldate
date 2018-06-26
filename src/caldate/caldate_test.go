113a0ecbae406f631c9de3cc15f3b09ea4a3e9b5
Pawat Nakpiphatkul <pawat.n@kasikornbank.com>
2018-06-26 15:37:03 +0700

    [Added]: ResultDay and Test_ResultDay_Input_StartDate_4_1_2018_EndDate_4_7_2018_Should_Be_182

--- src/caldate/caldate_test.go
+++ src/caldate/caldate_test.go
@@ -1,1 +1,13 @@
 package caldate
+
+import "testing"
+
+func Test_ResultDay_Input_StartDate_4_1_2018_EndDate_4_7_2018_Should_Be_182(t *testing.T) {
+	startDate := date{Date: 4, Month: 1, Year: 2018}
+	endDate := date{Date: 4, Month: 7, Year: 2018}
+	expected := 182
+	result := ResultDay(startDate, endDate)
+	if expected != result {
+		t.Errorf("expected %d but get %d", expected, result)
+	}
+}
