cc26a0c909340521b3509decb0289f06e2f59ff7
Dearrays <dear_180771@hotmail.com>
2018-06-27 11:47:53 +0700

    [Edited] Interface HTML and javascript

---
+++ html/appconfig.js
@@ -0,0 +1,25 @@
+$(function () {
+  $("button").click(function () {
+
+    // var showData = $('#output');
+    //alert("65555");
+
+    $.getJSON("example.json", function (data) {
+      console.log(data)
+      // var items = data.items.map(function (item) {
+      //   return item.key + ': ' + item.value;
+      // });
+
+      // showData.empty();
+
+      // if (items.length) {
+      //   var content = '<li>' + items.join('</li><li>') + '</li>';
+      //   var list = $('<ul />').html(content);
+      //   showData.append(list);
+      //   alert(2);
+      // }
+    });
+
+    // showData.text('Loading the JSON file.');
+  });
+});
