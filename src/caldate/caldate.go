d74814f4dad4d9ff0d7fcc73e526b81b879d6865
atchapcyp <atcha.crypto@gmail.com>
2018-06-27 11:18:19 +0700

    [Added] test

--- src/caldate/caldate.go
+++ src/caldate/caldate.go
@@ -26,6 +26,7 @@ func ResultDay(startDate, endDate date) int {
 	startTime := time.Date(startDate.Year, time.Month(startDate.Month), startDate.Date, 0, 0, 0, 0, time.UTC)
 	endTime := time.Date(endDate.Year, time.Month(endDate.Month), endDate.Date, 0, 0, 0, 0, time.UTC)
 	diff := endTime.Sub(startTime)
+	//kkk := time
 	return int(diff.Hours()/24) + 1
 }
 
@@ -35,4 +36,59 @@ func convertToSecond(days int) uint64 {
 
 func convertToMin(second uint64) uint64 {
 	return second / 60
+}
+
+type DetailStruct struct {
+	Year, Month, Day int
+}
+
+func (a date) ResultDetailSameYear(endDate date) bool {
+	return a.Year == endDate.Year
+}
+
+func diff(a, b time.Time) (year, month, day, hour, min, sec int) {
+	if a.Location() != b.Location() {
+		b = b.In(a.Location())
+	}
+	if a.After(b) {
+		a, b = b, a
+	}
+	y1, M1, d1 := a.Date()
+	y2, M2, d2 := b.Date()
+
+	h1, m1, s1 := a.Clock()
+	h2, m2, s2 := b.Clock()
+
+	year = int(y2 - y1)
+	month = int(M2 - M1)
+	day = int(d2 - d1)
+	hour = int(h2 - h1)
+	min = int(m2 - m1)
+	sec = int(s2 - s1)
+
+	// Normalize negative values
+	if sec < 0 {
+		sec += 60
+		min--
+	}
+	if min < 0 {
+		min += 60
+		hour--
+	}
+	if hour < 0 {
+		hour += 24
+		day--
+	}
+	if day < 0 {
+		// days in month:
+		t := time.Date(y1, M1, 32, 0, 0, 0, 0, time.UTC)
+		day += 32 - t.Day()
+		month--
+	}
+	if month < 0 {
+		month += 12
+		year--
+	}
+
+	return
 }
